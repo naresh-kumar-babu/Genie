66c9b37f8eeef4a59681c92ff55a23391e6b0474
Naresh Kumar <nk2indian@gmail.com>
2021-10-06 11:11:37 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -7,29 +7,27 @@ from django.core.files.base import ContentFile
 
 @csrf_exempt
 def home(request):
+  result = {}
+  if request.method == 'POST':
     result = {}
-    if request.method == 'POST':
-        result = {}
-        """
-        # img = ContentFile(request.FILES.get('myFile').read())
-        if img == '':
-            result = {'error':'Image not provided'} 
-        else:
-            im = Image.open(img)
-            im = im.convert('RGB')
-            result = predict(im)
-        # print(result)
-        # return render(request, 'api/index.html', {'result': int(result), 'stat': True})
-    # return render(request, 'api/index.html', {'stat': False})
-        """
-        img_url = request.POST.get('url_input')
-        if img_url == '':
-            result = {'error':'Image not provided'}
-        else:
-            im = Image.open(requests.get(url_input, stream=True).raw)
-            im = im.convert('RGB')
-            result = predict(im)
-     print(result)
-     return HttpResponse(str(result),content_type='text/json')
-
-
+    """
+    # img = ContentFile(request.FILES.get('myFile').read())
+    if img == '':
+      result = {'error':'Image not provided'} 
+    else:
+      im = Image.open(img)
+      im = im.convert('RGB')
+      result = predict(im)
+      # print(result)
+      # return render(request, 'api/index.html', {'result': int(result), 'stat': True})
+  # return render(request, 'api/index.html', {'stat': False})
+    """
+    img_url = request.POST.get('url_input')
+    if img_url == '':
+      result = {'error':'Image not provided'}
+    else:
+      im = Image.open(requests.get(url_input, stream=True).raw)
+      im = im.convert('RGB')
+      result = predict(im)
+  print(result)
+  return HttpResponse(str(result),content_type='text/json')
